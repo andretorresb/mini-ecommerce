43c03580bc3974fbb839f9447bb444c4adb8faba
Andre Torres <andret44@icloud.com>
2026-04-27 21:25:05 -0400

    feat: configura tailwind css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,10 @@
 export function Login() {
-  return <h1>Login</h1>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-zinc-100 text-zinc-900">
+      <div className="rounded-2xl bg-white p-8 shadow-lg">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <p className="mt-2 text-zinc-600">Mini E-commerce FakeStore</p>
+      </div>
+    </main>
+  );
 }
